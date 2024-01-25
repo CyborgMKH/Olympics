06aea719e3f320204930df667334db1502734b53
Dipeshcdy <techdipesh36@gmail.com>
2024-01-25 17:48:20 +0545

    first commit

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,76 @@
+<footer class="footer  xl:px-20 lg:px-5 md:px-20 px-5 py-20 text-white font-[300]">
+    <div class="sm:grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+        <div class="">
+            <div class=" mx-auto mb-5">
+                <img class="w-full rounded-xl" src="{{asset('images/logo4.png')}}" alt="">
+            </div>
+            <p class="text-justify ">Being a "leading" IT company suggests that it is at the forefront of
+                the industry, setting trends, and often acting as a role model for other companies in
+                terms of performance, growth, and impact. Its consistent dedication to 
+                technological advancement and client satisfaction solidifies its position as the 
+                leading IT powerhouse in the country.</p>
+           
+        </div>
+        <div class=" flex mt-[70px]">
+            <div class="mx-auto ">
+                <h2 class="text-xl font-bold">QUICK LINKS</h2>
+                <ul class="text-lg mt-10 list-disc ml-5">
+                    <li class="mt-2"><a class="capitalize" href="/company/about">About Us</a></li>
+                    <li class="mt-2"><a class="capitalize" href="/services">Services</a></li>
+                    <li class="mt-2"><a class="capitalize" href="/company/teams">our teams</a></li>
+                    <li class="mt-2"><a class="capitalize" href="/company/clients">our clients</a></li>
+                    <li class="mt-2"><a class="capitalize" href="/contact">contact us</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="mt-[70px] flex">
+            <div class="mx-auto sm:mx-0">
+                <h1 class=" text-xl font-bold text-center sm:text-left ">Contact Information</h1>
+                <a href="mailto:info@nth.com.np" class="text-lg text-justify mt-10 flex"><i class="mr-3 my-auto  fa-regular fa-envelope"></i><span class="my-auto">info@nth.com.np</span></a>
+                <a href="tel:+9779813600949" class="text-lg mt-3 flex"><i class="mr-3 my-auto fa fa-phone"></i>+977 9761610962</a>
+                <a href="https://goo.gl/maps/sAUjXg52YjrH4vRg8" target="_blank" class="text-lg  mt-2 flex"><i class="mr-3 my-auto fa fa-map-marker"></i>Shantinagar, Kathmandu</a>
+            </div>
+        </div>
+        <div class="mt-[70px] flex">
+            <div class="sm:ml-auto mx-auto sm:mx-0">
+                <h1 class="text-xl text-center font-bold">Social Links</h1>
+                <div class="mt-16 flex-wrap">
+                    <div class="grid grid-cols-3 gap-3">
+
+                        <a href="https://www.facebook.com/amaginginfosys12" target="_blank" class="bg-gray-600 h-[80px] m-auto w-[80px] flex hover:bg-[#416ac3] transition-all ease-linear rounded-full">
+                            <i class="text-2xl m-auto fab  fa-facebook"></i>
+                        </a>
+                        <a href="https://www.instagram.com/amazinginfosys12/"  class="bg-gray-600 h-[80px] m-auto w-[80px] flex hover:bg-[#ee2a7b] transition-all ease-linear rounded-full ">
+                            <i class="fa-brands text-2xl fa-instagram m-auto"></i>
+                        </a>
+                        <a href="https://www.api.whatsapp.com/send?phone=%2B9779861686665"  class="bg-gray-600 h-[80px] m-auto w-[80px] flex  hover:bg-[#38a195] transition-all ease-linear rounded-full">
+                            <i class="fa-brands text-2xl fa-whatsapp m-auto"></i>
+                        </a>
+                       
+                        {{-- <a href=""  class=" w-[90px] h-[90px]">
+                            <img class="w-full h-full" src="{{asset('images/Log1234-03.png')}}" alt="">
+                        </a>
+                        <a href=""  class=" w-[90px] h-[90px]">
+                            <img src="{{asset('images/Log1234-07.png')}}" alt="">
+                        </a>
+                        <a href=""  class="w-[90px] h-[90px]">
+                            <img src="{{asset('images/Log1234-08.png')}}" alt="">
+                        </a> --}}
+                    </div>
+                    <div class="grid grid-cols-2 gap-3 mt-8">
+                        <a href="https://www.nth.com.np/limitless" class="hover:shadow-xl hover:shadow-red-600 rounded-xl duration-500 hover:-translate-y-2 bg-white px-5 py-2">
+                            <img class="rounded-xl mx-auto" src="{{asset('images/web/logo-limitless.png')}}" alt="">
+                        </a>
+                        <a href="" class="hover:shadow-xl hover:shadow-red-600 rounded-xl duration-500 hover:-translate-y-2 bg-white px-5 py-2">
+                            <img class="rounded-xl mx-auto" src="{{asset('images/logo3.png')}}" alt="">
+                        </a>
+                        <a href="https://www.nth.com.np/" class="hover:shadow-xl  col-span-2 hover:shadow-red-600 rounded-xl duration-500 hover:-translate-y-2 bg-white px-5 py-2">
+                            <img class="rounded-xl mx-auto" src="{{asset('images/web/logo-digital.jpg')}}" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,26 @@
+{{-- nav bar --}}
+<nav id="navbar" class=" navbar py-3 bg-transparent  transition-all ease-linear  fixed top-0 left-0 right-0 w-full z-[999]">
+    <div class="flex  xl:px-20  md:px-20 px-5 lg:px-5  justify-between w-full">
+        <div class="mobile:w-[250px] w-[500px] z-[999]">
+            <a href="" ><img class="w-full" src="{{asset('images/logo2.png')}}" alt=""></a>
+        </div>
+        <div id="mobnav" class="text-[15px] lg:flex md:px-20 px-5 lg:px-0 md:text-lg mx-0 mobnav  pt-24 lg:pt-0 lg:w-full w-[80%]">
+            <div class="lg:m-auto">
+                <ul class="flex   nav-container">
+                    <li class="mx-4 my-2 lg:my-auto "><a href=""  class="{{ request()->is('/')?'active':''}} nav-link">Home</a></li>
+                    <li class="mx-4 my-2 lg:my-auto "><a href=""  class=" nav-link">Olympic Games</a></li>
+                    <li class="mx-4 my-2 lg:my-auto "><a href=""  class="nav-link">Athletes</a></li>
+                    <li class="mx-4 my-2 lg:my-auto "><a href=""  class="nav-link">Sports</a></li>
+                    <li class="mx-4 my-2 lg:my-auto "><a href=""  class="nav-link">News</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="mt-3 lg:hidden mobile-icon cursor-pointer" id="mobile-icon" onclick="menuBtn()" >
+            <span class="line-1"></span>
+            <span class="line-2"></span>
+            <span class="line-3"></span>
+        </div>
+    </div>
+</nav>
+{{-- end of nav bar --}}
