a79dc593bbba3987676226b650f81038bb3315e7
Dipeshcdy <techdipesh36@gmail.com>
2024-02-21 23:31:29 +0545

    final responsive home page and layout

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -21,7 +21,7 @@ class CountryResource extends Resource
 {
     protected static ?string $model = Country::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-globe-europe-africa';
 
     public static function form(Form $form): Form
     {
