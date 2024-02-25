3eef94258c226f8e44d957f96cd923078acbb574
cyborgmkh <sumanmukhiya@ismt.edu.np>
2024-02-25 16:29:42 +0545

    added rout in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,12 +33,16 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-3" href="{{ route('register') }}">
+                {{ __("Don't have an account?") }}
+            </a>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
+            
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
