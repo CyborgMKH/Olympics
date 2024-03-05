a68c2774153697b78991e066961be8c8bc3d0663
Dipeshcdy <techdipesh36@gmail.com>
2024-03-05 22:23:24 +0545

    :bug: Middleware fixed of admin

--- routes/web.php
+++ routes/web.php
@@ -31,32 +31,34 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/search',[SearchController::class,'index'])->name('search');
-Route::get('/news',[NewsController::class,'index'])->name('news');
-Route::get('/news/{slug}',[NewsController::class,'singleNews'])->name('news.single');
-Route::get('/athletes',[AthleteController::class,'index'])->name('athlete');
-Route::get('/athletes/{slug}',[AthleteController::class,'singleAthlete'])->name('athlete.single');
-Route::get('/highlights',[HighlightController::class,'index'])->name('highlight');
-Route::get('/highlights/{slug}',[HighlightController::class,'single'])->name('highlight.single');
-Route::get('/sports',[SportController::class,'index'])->name('sport');
-Route::get('/events',[EventController::class,'index'])->name('event');
-Route::post('/news/like/{id}',[NewsController::class,'newsLike']);
-Route::post('/news/unlike/{id}',[NewsController::class,'newsUnlike']);
+Route::fallback(function () {
+    return view('404');
+});
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/news', [NewsController::class, 'index'])->name('news');
+Route::get('/news/{slug}', [NewsController::class, 'singleNews'])->name('news.single');
+Route::get('/athletes', [AthleteController::class, 'index'])->name('athlete');
+Route::get('/athletes/{slug}', [AthleteController::class, 'singleAthlete'])->name('athlete.single');
+Route::get('/highlights', [HighlightController::class, 'index'])->name('highlight');
+Route::get('/highlights/{slug}', [HighlightController::class, 'single'])->name('highlight.single');
+Route::get('/sports', [SportController::class, 'index'])->name('sport');
+Route::get('/events', [EventController::class, 'index'])->name('event');
+Route::post('/news/like/{id}', [NewsController::class, 'newsLike']);
+Route::post('/news/unlike/{id}', [NewsController::class, 'newsUnlike']);
 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::get('/tickets',[TicketController::class,'index'])->name('ticket');
+
+    Route::get('/tickets', [TicketController::class, 'index'])->name('ticket');
     Route::get('create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
     Route::post('process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
     Route::get('success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
     Route::get('cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
-
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
